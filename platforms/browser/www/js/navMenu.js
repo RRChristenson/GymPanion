d4cf82ebef741526faa25b767947210433ece867
Rob Christenson <RRChristenson@live.com>
2017-09-01 13:37:13 -0500

    added form check home page, started profile page

---
+++ platforms/browser/www/js/navMenu.js
@@ -0,0 +1,65 @@
+document.write('\
+<div class="w-nav navbar" data-collapse="all" data-animation="over-left" data-duration="400" data-contain="1" data-easing="ease-out-quint" data-no-scroll="1">\
+<div class="w-container">\
+<nav class="w-nav-menu nav-menu bg-gradient" role="navigation">\
+    <div class="nav-menu-header">\
+      <div class="logo">GymPanion</div>\
+    </div>\
+    <a class="w-clearfix w-inline-block nav-menu-link" href="home.html" data-load="1">\
+      <div class="icon-list-menu">\
+        <div class="icon ion-home"></div>\
+      </div>\
+      <div class="nav-menu-titles">Home Page</div>\
+    </a>\
+    <a class="w-clearfix w-inline-block nav-menu-link" href="formCheck.html" data-load="1">\
+        <div class="icon-list-menu">\
+          <div class="icon ion-checkmark"></div>\
+        </div>\
+        <div class="nav-menu-titles">Form Checks</div>\
+    </a>\
+    <a class="w-clearfix w-inline-block nav-menu-link" href="profile.html" data-load="1">\
+      <div class="icon-list-menu">\
+        <div class="icon ion-ios-contact-outline"></div>\
+      </div>\
+        <div class="nav-menu-titles">My Profile</div>\
+    </a>\
+    <a class="w-clearfix w-inline-block nav-menu-link" href="gymFinder.html" data-load="1">\
+      <div class="icon-list-menu">\
+        <div class="icon ion-ios-location"></div>\
+      </div>\
+      <div class="nav-menu-titles">Gym Finder</div>\
+    </a>\
+    <a class="w-clearfix w-inline-block nav-menu-link" href="spotter.html" data-load="1">\
+      <div class="icon-list-menu">\
+         <div class="icon ion-ios-eye"></div>\
+      </div>\
+      <div class="nav-menu-titles">Need a Spotter?</div>\
+    </a>\
+    <a class="w-clearfix w-inline-block nav-menu-link" href="Routines.html" data-load="1">\
+      <div class="icon-list-menu">\
+        <div class="icon ion-ios-pulse-strong"></div>\
+      </div>\
+        <div class="nav-menu-titles">Get a Routine</div>\
+    </a>\
+    <a class="w-clearfix w-inline-block nav-menu-link" href="Coaching.html" data-load="1">\
+      <div class="icon-list-menu">\
+        <div class="icon ion-ios-people"></div>\
+      </div>\
+        <div class="nav-menu-titles">Coaching</div>\
+    </a>\
+    <div class="separator-bottom"></div>\
+    <div class="separator-bottom"></div>\
+    <div class="separator-bottom"></div>\
+  </nav>\
+<div class="wrapper-mask" data-ix="menu-mask"></div>\
+<div class="w-nav-button navbar-button left" id="menu-button" data-ix="hide-navbar-icons">\
+<div class="navbar-button-icon home-icon">\
+  <div class="bar-home-icon top"></div>\
+  <div class="bar-home-icon middle"></div>\
+  <div class="bar-home-icon bottom"></div>\
+</div>\
+</div>\
+</nav>\
+</div>\
+</div>\
+');
---
+++ www/js/formcheck.js
@@ -0,0 +1,45 @@
+function getRecent(){
+    let debug = document.getElementById("debug");
+    /*debug.innerHTML="Hellooooooo Test";*/
+    $.ajax({
+        
+               type : "GET",
+               url : "http://192.168.1.3:5000/getRecent",
+               dataType : 'json',
+               /*jsonpCallback : getRecentJsonParse(), // the function to call*/
+               success: function(response){
+                let div = document.getElementById("FormBg1");
+                let debug = document.getElementById("debug");
+                debug.innerHTML=response;
+                div.style.backgroundImage = "url('"+response+"')";
+               },
+               error : function(xhr, ppp,rrr) {
+                   alert(xhr+"Errr is occured"+ppp+rrr);
+               }
+           });
+
+    /*cordovaFetch('http://192.168.1.3:5000/getRecent')
+    .then(function(response) {
+        let div = document.getElementById("FormBg1");
+        let debug = document.getElementById("debug");
+        debug.innerHTML="Hellooooooo Test";
+        var data = this.responseData.DATA;
+        div.style.backgroundImage = "url('"+data+"')";//reponse.json();
+      return response.json()*/
+};
+
+function getRecentJsonParse(data){
+    let div = document.getElementById("FormBg1");
+    let debug = document.getElementById("debug");
+    debug.innerHTML=data;
+//reponse.json();
+    /*$.each(json, function(key, value) {
+                debug.innerHTML=value;
+               div.style.backgroundImage = "url('"+value+"')";
+               //tempOpt = '<option value="' + key + '">' + value + '</option>';
+               //mylist.append(tempOpt)
+               //mylist.selectmenu("refresh");
+        
+           });*/
+
+};
